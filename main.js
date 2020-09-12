d27db8505d7672e9b87a3e3986fb00f2dd64b4bc
symmx <70875582+symmx@users.noreply.github.com>
2020-09-12 19:28:21 +0700

    main.js
    this just a basic script and for fun purpose 👍

---
+++ main.js
@@ -0,0 +1,34 @@
+let userName = 'Jean'
+
+userName ?
+console.log(`Hello, ${userName}!`) :
+console.log('Hello stranger!');
+
+const userQuestion = 'Can i be rich?'
+  if (userName = userName) {
+    console.log(`${userName} asking: ${userQuestion}`);
+  }
+
+const randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = ''
+
+  console.log(`Magic 8-ball says ${eightBall}`)
+  console.log(randomNumber);
+  if (randomNumber === 0) {
+    console.log('It is certain')
+  } else if (randomNumber === 1){
+    console.log('It is decidedly so');
+  } else if (randomNumber === 2) {
+    console.log('Reply hazy try again');
+  } else if (randomNumber === 3) {
+    console.log('Cannot predict now');
+  } else if (randomNumber === 4) {
+    console.log('Do not count on it');
+  } else if (randomNumber === 5) {
+    console.log('Outlook not so good');
+  } else if (randomNumber === 6) {
+    console.log('Signs point to yes');
+  } else if (randomNumber === 7) {
+    console.log('Try again');
+  }
